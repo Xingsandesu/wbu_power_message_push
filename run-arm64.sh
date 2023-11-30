09a251e11e1823ec22679d01c52c04e85df56336
Fushinn <55939146+Xingsandesu@users.noreply.github.com>
2023-11-30 18:10:14 +0800

    Create run-arm64.sh

---
+++ run-arm64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 设置工作目录
+target_directory="/home/linaro/wbu_power_api_runtime"
+
+# 切换到目标目录
+cd "$target_directory"
+
+docker run -itd \
+    --privileged \
+    --network bridge \
+    --rm \
+    --name power \
+    -p 8080:8080 \
+    -v "$(pwd)"/config.yaml:/app/config.yaml \
+    -v "$(pwd)"/.power.yaml:/app/.power.yaml \
+    -v "$(pwd)"/adb_logs:/app/adb_logs \
+    -v "$(pwd)"/main_logs:/app/main_logs \
+    -v "$(pwd)"/proxy_logs:/app/proxy_logs \
+    -v "$(pwd)"/keys:/root/.android \
+    fushin/wbupowerapi:1.1-arm64
